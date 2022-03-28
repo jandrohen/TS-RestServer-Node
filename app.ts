5cc2d38d67c7596c5b2f099b14fdef848ac2c3ab
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-28 20:20:46 +0200

    (lesson 242 course node) Create server express and config initial * Install cors, dotenv, and express

--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
-export const name = 'Jandro';
-console.log(name)
+import dotenv from 'dotenv';
+import Server from './models/server';
+
+// Configurar dot.env
+dotenv.config();
+
+
+const server = new Server();
+
+server.listen();
