a772907845e9b7d3752b026e566dd0906048fd76
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-04-17 20:51:59 +0200

    (lesson 243-244 course node) Explanation nodemon, TypeScript compilation in development time and routes App * Add Crud entity Users

--- models/server.ts
+++ models/server.ts
@@ -1,13 +1,25 @@
 import express, { Application }  from 'express';
 
+import userRoutes from '../routes/user';
+
 class Server {
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        users: '/api/users'
+    };
 
     constructor() {
         this.app  = express();
-        this.port = process.env.PORT || '8000';
+        this.port = process.env.PORT || '8080';
+
+        //Definition routes
+        this.routes();
+    }
+
+    routes() {
+        this.app.use( this.apiPaths.users, userRoutes)
     }
 
     listen(){
